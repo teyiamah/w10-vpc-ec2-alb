1f818b1880a1df311520eb009ebde0806b1e4931
amah <kangni1993@gmail.com>
2024-09-25 20:40:35 -0400

    terraform week10

---
+++ sg.tf
@@ -0,0 +1,52 @@
+resource "aws_security_group" "sg-demo" {
+  name        = "webserver-sg"
+  vpc_id      = aws_vpc.vp1.id
+  description = "Allow httpd"
+
+  ingress {
+    description = "allow http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    env = "Dev"
+  }
+  depends_on = [aws_vpc.vp1]
+}
+
+#security group for ec2
+
+resource "aws_security_group" "sg-demo1" {
+  name        = "web-sg"
+  vpc_id      = aws_vpc.vp1.id
+  description = "Allow httpd"
+
+  ingress {
+    description = "allow http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    // cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.sg-demo.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    env = "Dev"
+  }
+  depends_on = [aws_vpc.vp1, aws_security_group.sg-demo]
+}
